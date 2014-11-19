a28bd7bcd464d01d75e589e1de842dd620ef093b
Chase Floied <CFhotwing@Chases-MacBook-Air.local>
2014-11-18 22:38:14 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,9 +15,6 @@
 }
 
 // put new methods here
-character = (Character*)[CCBReader load:@"Character"];
-[CCPhysicsNode addChild:character];
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen [character flap];
     
 }
